d9e176f87436325b470f2c03b967535db6d4fa38
Zach Fogg <zach.fogg@gmail.com>
2013-09-07 18:29:31 -0400

    Switched to GoogleMaps v3.

--- code/umagellan/UMagellan/static/js/index.js
+++ code/umagellan/UMagellan/static/js/index.js
@@ -1,17 +1,14 @@
-var map,
-    layer;
-    lon = -76.9416,
-    lat = 38.9857;
+var map;
 
-$(function(){
-  map = new OpenLayers.Map( 'map');
-  layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
-  map.addLayer(layer);
-  map.setCenter(
-      new OpenLayers.LonLat(lon, lat).transform(
-        new OpenLayers.Projection("EPSG:4326"),
-        map.getProjectionObject()
-        ), 16
-      );
-});
+// Init map.
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 

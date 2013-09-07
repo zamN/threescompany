064b240f56ce656507659214017df3e08174c043
Craig Palermo <cpalermo2@gmail.com>
2013-09-07 12:56:40 -0400

    Setup django project

---
+++ code/umagellan/UMagellan/UMagellan/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'UMagellan.views.home', name='home'),
+    # url(r'^UMagellan/', include('UMagellan.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)

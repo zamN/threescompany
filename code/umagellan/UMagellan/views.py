927a7d04927a88ef8aec8e2d86c3f0205351af1a
Craig Palermo <cpalermo2@gmail.com>
2013-09-07 16:38:50 -0400

    Added shitload of font files

--- code/umagellan/UMagellan/views.py
+++ code/umagellan/UMagellan/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext, loader
+from bs4 import BeautifulSoup
 
 # views go here
 def index(request):
+    soup = BeautifulSoup("bla")
     return render_to_response('example.html', {}, context_instance=RequestContext(request))

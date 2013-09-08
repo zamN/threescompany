34139a765a5164737cd50ab76943ffd3d2113f74
Zach Fogg <zach.fogg@gmail.com>
2013-09-08 01:51:12 -0400

    Fixed adding classes.

--- code/umagellan/UMagellan/static/js/courses.js
+++ code/umagellan/UMagellan/static/js/courses.js
@@ -1,25 +1,36 @@
 $(function() {
-    $('.save-course').click(function(e) {
-        var id = $(this).parents('.tab-pane').attr('id');
+    function saveCourse(e) {
+        var id = $('.tab-pane.active').attr('id');
         var course_name = $('#' + id + ' .name-field').val();
         var course_sec = $('#' + id + ' .section-field').val();
 
         // Append course to table
         if(course_name.length != 0 && course_sec.length != 0) {
-            $(this).parents('.tab-pane').children('table').append(
+            $('.tab-pane.active').children('table').append(
+                '<tr>' +
                 '<td>' + course_name + '</td>' +
                 '<td>' + course_sec + '</td>' +
-                '<td width="10%"><a href="#" class="rem-course">&times;</a></td>'
+                '<td width="10%"><a href="#" class="rem-course">&times;</a></td>' +
+                '</tr>'
             );
         }
 
+        var latestCourseRow = $('.tab-pane.active table tr:last-child');
+        window.x = latestCourseRow();
         // Re-apply remove events
-        $('.rem-course').click(function(e) {
-            $(this).parents('tr').remove();
+        console.log(latestCourseRow.children);
+        latestCourseRow.children(".rem-course").click(function(e) {
+            latestCourseRow.remove();
         });
 
         // Prevent default action
         e.preventDefault();
+    }
+    $(".save-course").click(saveCourse);
+    Mousetrap.bind("enter", function(e) {
+        if ($(".section-field").is(":focus")) {
+            saveCourse.call(this, e);
+        }
     });
 
     $('.rem-course').click(function(e) {
